5338e460c5262cb0100264381d5066c9702740a9
senseiGai <swerss564@gmail.com>
2025-02-22 22:34:42 +0500

    fix: routing

--- spec-app/App.tsx
+++ spec-app/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import { AppNavigator } from "./AppNavigator";
+import { AppNavigator } from "./app/AppNavigator";
 
 export default function App() {
     return (
